e3c65d40490ba4bfbfc00b2c38ece287121d59fd
ngtruongtanquyenn <tanquyen06012004@gmail.com>
2024-10-07 15:40:46 +0700

    Delete BaiTap7a directory

--- BaiTap7a/BaiTap7a/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BaiTap7a.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
